f9441688f4c873a0c8db8d469c6bea5a125961b6
quantmatt <matthew.bailey@enzymelabs.com.au>
2020-10-14 10:40:42 +1100

    Updated settings to use environ variables

--- ELLIMS/settings.py
+++ ELLIMS/settings.py
@@ -91,8 +91,8 @@ WSGI_APPLICATION = 'ELLIMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'eldb',
-        'USER': 'postgres',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
         'HOST': os.environ['DB_HOST'],
         'PORT': '5432'
