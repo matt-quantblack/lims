e6cd9560a40a347da7818fdf15f347687e5fd0e8
quantmatt <matthew.bailey@enzymelabs.com.au>
2020-10-14 21:45:05 +1100

    Updated settings to use environ variables

--- ELLIMS/settings.py
+++ ELLIMS/settings.py
@@ -146,7 +146,7 @@ if USE_S3:
     AWS_SECRET_ACCESS_KEY = os.getenv('FELIX_AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('FELIX_AWS_USER_ARN')
     AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME#f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     STATIC_LOCATION = 'static'
