623cc7355d5e68575cafded1ebf9f4cdc3836bd0
quantmatt <matthew.bailey@enzymelabs.com.au>
2020-10-14 12:27:25 +1100

    Updated settings to use environ variables

--- ELLIMS/settings.py
+++ ELLIMS/settings.py
@@ -34,7 +34,7 @@ if os.environ['DEBUG'] == 'True':
 else:
     DEBUF = False
 
-ALLOWED_HOSTS = ["10.1.1.230", "127.0.0.1", "10.1.1.7"]
+ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOSTS'], "10.1.1.230", "127.0.0.1", "10.1.1.7"]
 
 
 # Application definition
